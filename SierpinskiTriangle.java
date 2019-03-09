0ffeef8f947af487b0fc7fe72fdd89e93ad4fa8e
jlargs64 <justinlargo7@gmail.com>
2019-03-09 13:33:11 -0500

    Methods and proper data santization

--- SierpinskiTriangle.java
+++ SierpinskiTriangle.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 /**
  * Prints a Sierpinski Triangle iteratively or recursively to the screen.
@@ -9,7 +10,10 @@ import java.awt.*;
  */
 
 public class SierpinskiTriangle extends JPanel {
-    int width, height;
+    // Instance variables
+    private static final long serialVersionUID = 4742367089554814358L;
+    private int width, height;
+    private Random r = new Random();
 
     public SierpinskiTriangle() {
         super();
@@ -21,25 +25,69 @@ public class SierpinskiTriangle extends JPanel {
         setBackground(Color.black);
     }
 
-    public static void generateIteratively() {
+    public static void generateIteratively(int points) {
 
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                // Create and set up the window.
+                JFrame frame = new JFrame("Rainbow");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                SierpinskiTriangle panel = new SierpinskiTriangle();
+                frame.getContentPane().add(panel);
+
+                // Display the window.
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
     }
 
-    public static void generateRecursively() {
+    public static void generateRecursively(int points) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                // Create and set up the window.
+                JFrame frame = new JFrame("Sierpinski Triangle");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+                SierpinskiTriangle panel = new SierpinskiTriangle();
+                frame.getContentPane().add(panel);
+
+                // Display the window.
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(new Color(255,215,0));
+        //Top point
+        g.fillArc(width/2,0,1,1,0,360);
+        //Bottom left point
+        g.fillArc(width,height,1,1,0,360);
+        //Bottom Right point
+        g.fillArc(width,height,1,1,0,360);
     }
 
     public static void main(String[] args) {
 
-        String generationType = JOptionPane.showInputDialog(null, "Generate A Sierpinski Triangle",
-                "Generate iteratively or recursively?", JOptionPane.QUESTION_MESSAGE);
+        // Sanitize data and set instance variables
+        String generationType = JOptionPane.showInputDialog(null, "Iterative or Recursive: ",
+                "Generate A Sierpinski Triangle", JOptionPane.QUESTION_MESSAGE);
         ;
+        String strPoints = JOptionPane.showInputDialog(null, "Amount of points: ", "Generate A Sierpinski Triangle",
+                JOptionPane.QUESTION_MESSAGE);
+        int points = Integer.parseInt(strPoints);
+
         if (generationType.equalsIgnoreCase("iterative")) {
 
-            generateIteratively();
+            generateIteratively(points);
         } else if (generationType.equalsIgnoreCase("recursive")) {
-            
-            generateRecursively();
+
+            generateRecursively(points);
         } else {
             System.err.println("usage error: generation type must be iterative or recursive");
         }
