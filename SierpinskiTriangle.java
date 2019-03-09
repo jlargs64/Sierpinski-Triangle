520c1dd62242456f2bfc203553a4f0640a661181
jlargs64 <justinlargo7@gmail.com>
2019-03-09 13:50:40 -0500

    Iterative finished

--- SierpinskiTriangle.java
+++ SierpinskiTriangle.java
@@ -13,24 +13,25 @@ public class SierpinskiTriangle extends JPanel {
     // Instance variables
     private static final long serialVersionUID = 4742367089554814358L;
     private int width, height;
+    private static int points;
     private Random r = new Random();
 
     public SierpinskiTriangle() {
         super();
         setOpaque(true);
         setBackground(Color.BLACK);
-        setPreferredSize(new Dimension(300, 300));
+        setPreferredSize(new Dimension(800, 600));
         width = getPreferredSize().width;
         height = getPreferredSize().height;
         setBackground(Color.black);
     }
 
-    public static void generateIteratively(int points) {
+    public static void generateIteratively() {
 
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 // Create and set up the window.
-                JFrame frame = new JFrame("Rainbow");
+                JFrame frame = new JFrame("Sierpinski Triangle");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
                 SierpinskiTriangle panel = new SierpinskiTriangle();
@@ -65,24 +66,55 @@ public class SierpinskiTriangle extends JPanel {
         super.paintComponent(g);
         g.setColor(new Color(255,215,0));
         //Top point
+        Point c1 = new Point(width/2,0);
         g.fillArc(width/2,0,1,1,0,360);
         //Bottom left point
-        g.fillArc(width,height,1,1,0,360);
+        Point c2 = new Point(0,height);
+        g.fillArc(0,height,1,1,0,360);
         //Bottom Right point
+        Point c3 = new Point(width,height);
         g.fillArc(width,height,1,1,0,360);
+
+        //First point located inside triangle
+        Point p = new Point(getWidth()/2, getHeight() /2);
+        g.fillArc(p.x,p.y,1,1,0,360);
+
+        //Generate the triangle fractals
+        for(int i = 0; i < points; i++){
+            int newPoint = r.nextInt(3) + 1;
+            
+            if(newPoint == 1){
+
+                int midx = (p.x + c1.x)/2;
+                int midy = (p.y + c1.y)/2;
+                p = new Point(midx, midy);
+                g.fillArc(p.x, p.y, 1, 1, 0, 360);
+            }else if(newPoint == 2){
+
+                int midx = (p.x + c2.x)/2;
+                int midy = (p.y + c2.y)/2;
+                p = new Point(midx, midy);
+                g.fillArc(p.x, p.y, 1, 1, 0, 360);
+            }else{
+                int midx = (p.x + c3.x)/2;
+                int midy = (p.y + c3.y)/2;
+                p = new Point(midx, midy);
+                g.fillArc(p.x, p.y, 1, 1, 0, 360);
+            }
+        }
     }
 
     public static void main(String[] args) {
 
         // Sanitize data and set instance variables
-        String generationType = JOptionPane.showInputDialog(null, "Iterative or Recursive: ",
-                "Generate A Sierpinski Triangle", JOptionPane.QUESTION_MESSAGE);
-        ;
+        //String generationType = JOptionPane.showInputDialog(null, "Iterative or Recursive: ",
+               // "Generate A Sierpinski Triangle", JOptionPane.QUESTION_MESSAGE);
+        //;
         String strPoints = JOptionPane.showInputDialog(null, "Amount of points: ", "Generate A Sierpinski Triangle",
                 JOptionPane.QUESTION_MESSAGE);
-        int points = Integer.parseInt(strPoints);
-
-        if (generationType.equalsIgnoreCase("iterative")) {
+        points = Integer.parseInt(strPoints);
+        generateIteratively();
+        /*if (generationType.equalsIgnoreCase("iterative")) {
 
             generateIteratively(points);
         } else if (generationType.equalsIgnoreCase("recursive")) {
@@ -90,6 +122,6 @@ public class SierpinskiTriangle extends JPanel {
             generateRecursively(points);
         } else {
             System.err.println("usage error: generation type must be iterative or recursive");
-        }
+        }*/
     }
 }
